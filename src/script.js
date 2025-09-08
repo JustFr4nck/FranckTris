3971b0e51ace283ff6a1a778b1597e0bf680f1ef
JustFr4nck <francyperry07@gmail.com>
2025-09-08 12:57:05 +0200

    style setup

---
+++ src/script.js
@@ -0,0 +1,14 @@
+let arr = [];
+
+for(let i = 0; i < 9; i++){
+
+    arr[i] = document.getElementById("cel" + (i + 1));
+}
+
+
+
+function putOnClick(){
+
+
+}
+addEventListener("click", putOnClick);
