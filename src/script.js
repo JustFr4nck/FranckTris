de14b203a53825989ccaaf96cc29ca63036c00e9
JustFr4nck <francyperry07@gmail.com>
2025-09-08 14:48:49 +0200

    starting interaction update

--- src/script.js
+++ src/script.js
@@ -1,14 +1,28 @@
 let arr = [];
 
+let p1 = true;
+let p2 = false;
+
+const p1Img = `<img class="pImgStyle" src="../public/images/cat.png"></img>`;
+const p2Img = `<img src="../public/images/dog.png"></img>`;
+
 for(let i = 0; i < 9; i++){
 
     arr[i] = document.getElementById("cel" + (i + 1));
 }
 
 
-
 function putOnClick(){
 
+    if(p1 === true){
+
+        arr[1].innerHTML = p1Img;
+
+    }
+    else{
+
+    }
 
 }
-addEventListener("click", putOnClick);
+
+putOnClick();
